7433070869d37ad56ded405200252a0939cf746d
Phil <phil@upvalue.io>
2025-10-11 16:55:45 -0700

    os: update hello world message

--- os/src/wasm32/kernel.c
+++ os/src/wasm32/kernel.c
@@ -4,4 +4,4 @@ extern void host_putchar(char c);
 
 void putchar(char c) { host_putchar(c); }
 
-void kernel_enter() { printf("hello world from wasm\n"); }
+void kernel_enter() { printf("hello world from wasm2\n"); }
